d89916b57a7a1a6934d03110a9435a0ab3f59b36
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-12-17 23:59:12 +0100

    Change index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
-function Phone(brand, price, color, inches) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-	this.inches = inches;
-	// this.blablabla = function() {
-	// 	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", inches is " + this.inches + ", and the price is " + this.price + ".");
-	// }
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", inches is " + this.inches + ", and the price is " + this.price + ".");
+function Button(text) {
+	this.text = text || 'Hello';
+
 }
 
-var SamsungGalaxyS6 = new Phone("Samsung", 2200, "black", 5.5);
-var Iphone6S = new Phone("Apple", 2250, "silver", 5.3);
-var OnePlusOne = new Phone("OnePlus", 1600, "black", 4.9);
-
-SamsungGalaxyS6.printInfo();
-Iphone6S.printInfo();
-OnePlusOne.printInfo();
-
-Phone.prototype.Inches = function() {
-	console.log("The phone is " + this.brand + " inches is " + this.inches + ".");
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text)
+		});
+		$('body').append(this.$element);
+	}
 }
-Iphone6S.Inches();
+var btn1 = new Button('Hello!');
+btn1.create();
+
+var btn2 = new Button('Hello, hello hello!!!')
+btn2.create();
